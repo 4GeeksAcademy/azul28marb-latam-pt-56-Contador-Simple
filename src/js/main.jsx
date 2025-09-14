9fd6c23f75f8719d7020f2049c783bf0d0b203ec
Azul MA <azulma2826@gmail.com>
2025-09-14 03:11:57 +0000

    first commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import PropTypes from 'prop-types'
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,8 +13,39 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+function SimpleCounter(props) {
+  return (<div className='bigCounter'>
+    <div className='calendar'>
+      <i className="fa-regular fa-clock"></i>
+    </div>
+    <div className='five'>{props.digitFive % 10}</div>
+    <div className='four'>{props.digitFour % 10}</div>
+    <div className='three'>{props.digitThree % 10}</div>
+    <div className='two'>{props.digitTwo % 10}</div>
+    <div className='one'>{props.digitOne % 10}</div>
+  
+  </div>)
+}
+
+SimpleCounter.propTypes = {
+  digitFive: PropTypes.number,
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number
+}
+
+let counter = 0;
+setInterval(function() {
+    const five = Math.floor(counter/10000)
+    const four = Math.floor(counter/1000)
+    const three = Math.floor(counter/100)
+    const two = Math.floor(counter/10)
+    const one = Math.floor(counter/1)
+    counter++;
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five}/>
+      </React.StrictMode>,
+    )
+},1000)
